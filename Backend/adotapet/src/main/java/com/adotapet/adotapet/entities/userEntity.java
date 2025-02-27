45779be4b1ca91f419625c70d6e731c0eb8ec26c
Eryancosta <eryan.costa@hotmail.com>
2025-02-27 14:28:57 -0300

    userEntity

---
+++ Backend/adotapet/src/main/java/com/adotapet/adotapet/entities/userEntity.java
@@ -0,0 +1,18 @@
+package com.adotapet.adotapet.entities;
+
+
+public class userEntity {
+    private String name;
+
+    Integer id;
+    private String email;
+    private String password;
+    private String telefone;
+    private String cpf;
+    private String address;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String img;
+
+}
