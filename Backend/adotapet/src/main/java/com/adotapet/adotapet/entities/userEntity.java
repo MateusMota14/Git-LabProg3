fa2bb9553459b99a45095628f828d3cabc153280
Eryancosta <eryan.costa@hotmail.com>
2025-03-06 20:20:33 -0300

    banco de dados up

--- Backend/adotapet/src/main/java/com/adotapet/adotapet/entities/userEntity.java
+++ Backend/adotapet/src/main/java/com/adotapet/adotapet/entities/userEntity.java
@@ -1,18 +1,57 @@
 package com.adotapet.adotapet.entities;
 
+// import java.time.LocalDateTime;
 
-public class userEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class UserEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
     private String name;
-
-    Integer id;
     private String email;
     private String password;
-    private String telefone;
-    private String cpf;
-    private String address;
-    private String city;
-    private String state;
-    private String postalCode;
-    private String img;
+    
 
+    public UserEntity() {} //para o JPA
+
+    public UserEntity(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ Backend/adotapet/src/main/java/com/adotapet/adotapet/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.adotapet.adotapet.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adotapet.adotapet.entities.UserEntity;
+// import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+
+    Iterable<UserEntity> findByEmail(String email);
+    UserEntity findByEmailAndPassword(String email, String password);
+}
