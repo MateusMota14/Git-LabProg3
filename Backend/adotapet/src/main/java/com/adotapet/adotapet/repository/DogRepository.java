5eca7f028eca66707341f96dc92257c80e0adfa3
Eryancosta <eryan.costa@hotmail.com>
2025-03-10 21:49:53 -0300

    dog controller

---
+++ Backend/adotapet/src/main/java/com/adotapet/adotapet/repository/DogRepository.java
@@ -0,0 +1,15 @@
+package com.adotapet.adotapet.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adotapet.adotapet.entities.DogEntity;
+
+
+@Repository
+public interface DogRepository extends CrudRepository<DogEntity, Integer> {
+
+    Optional<DogEntity> findById(Integer id);
+}
